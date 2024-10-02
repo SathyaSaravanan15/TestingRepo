7fe0728f8168cf6d4cb478d1eddeb7feed620771
SathyaSaravanan15 <sathyasaravanamurthy15@gmail.com>
2024-10-02 14:25:31 +0530

    QA1msg

--- src/org/logi/Login.java
+++ src/org/logi/Login.java
@@ -5,6 +5,8 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("msg to Master");
         System.out.println("msg to master from QA");
+        System.out.println("msg from QA1");
+        
 
 	}
 
